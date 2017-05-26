a590ead9a708327fb8ebb3a2016ba89c07128d3d
Jason Ainsworth <jasoncruze@gmail.com>
2017-05-26 16:00:17 -0700

    add in functionality to display a specific doctors picture and list out all the insurance providers that they accept

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -3,10 +3,9 @@ var Doctor = require('./../js/doctor.js').doctorModule;
 
 var displayAllDoctors = function(allDoctors) {
   var doctor = new Doctor();
-
-    $('#all-doctors ul').empty();
+  $('#all-doctors ul').empty();
   allDoctors.forEach(function(doctor){
-    $('#all-doctors ul').append(`<li class="click-doctor" value="${doctor.npi}"> Doctor: ${doctor.profile.first_name} ${doctor.profile.last_name} </li> `);
+    $('#all-doctors ul').append(`<li style="cursor: pointer" class="click-doctor" value="${doctor.npi}"> Doctor: ${doctor.profile.first_name} ${doctor.profile.last_name} </li> `);
     });
     $('.click-doctor').click(function(){
       doctorId = $(this).val();
@@ -15,16 +14,37 @@ var displayAllDoctors = function(allDoctors) {
 };
 
 var displayDoctor = function(doctor) {
-  console.log(doctor);
-  console.log(doctor.specialties);
-  $('#doctor-info').append(`<h3> ${doctor.profile.first_name} ${doctor.profile.last_name} </h3> <h4>${doctor.specialties[0].description}</h4><li> ${doctor.profile.bio} </li> <li>${doctor.practices[0].description}</li><strong>Phone Number: ${doctor.practices[0].phones[0].number} </strong> <a href="${doctor.profile.image_url}">View Image</a>`);
+  $('#doctor-info').append(`
+    <h3> ${doctor.profile.first_name} ${doctor.profile.last_name}</h3>
+    <h4>${doctor.specialties[0].description}</h4>
+    <li> ${doctor.profile.bio} </li>
+    <li>${doctor.practices[0].description}</li>
+    <strong>Phone Number: ${doctor.practices[0].phones[0].number} </strong>
+    <li style="color:blue; text-decoration: underline;"id="view-image">View Image</li>
+    <li style="color:blue; text-decoration: underline;"id="view-insurance">View Accepted Insurance</li>
+    `);
+  $('#view-image').click(function(){
+    openInNewTab(doctor.profile.image_url);
+  });
+  $('#view-insurance').click(function(){
+    doctor.insurances.forEach(function(insuranceCompany){
+      $('#show-insurance ul').append(`<li>${insuranceCompany.insurance_plan.name}</li>`);
+    });
+  });
+};
+
+function openInNewTab(url) {
+  var win = window.open(url, '_blank');
+  win.focus();
 };
 
 
 
 $(document).ready(function(){
+  $('#all-doctors').hide();
   var doctor = new Doctor();
   $('#submit-button').click(function(){
+    $('#all-doctors').show();
     $('#error').empty();
     var state = $('#state').val();
     var userQuery = $('#query').val();
