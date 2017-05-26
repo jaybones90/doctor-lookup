d1f110a7c6e902b9030b0967734a86c76629cc8a
Jason Ainsworth <jasoncruze@gmail.com>
2017-05-26 15:34:09 -0700

    show an error if no doctors are found based on the users search

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -3,6 +3,8 @@ var Doctor = require('./../js/doctor.js').doctorModule;
 
 var displayAllDoctors = function(allDoctors) {
   var doctor = new Doctor();
+
+    $('#all-doctors ul').empty();
   allDoctors.forEach(function(doctor){
     $('#all-doctors ul').append(`<li class="click-doctor" value="${doctor.npi}"> Doctor: ${doctor.profile.first_name} ${doctor.profile.last_name} </li> `);
     });
@@ -13,8 +15,9 @@ var displayAllDoctors = function(allDoctors) {
 };
 
 var displayDoctor = function(doctor) {
-  console.log(doctor.practices[0].phones[0].number)
-  $('#doctor-info').append(`<h3> ${doctor.profile.first_name} ${doctor.profile.last_name} </h3> <li> ${doctor.profile.bio} </li> <li>Phone Number: ${doctor.practices[0].phones[0].number} </li> <a href="${doctor.profile.image_url}">View Image</a>`)
+  console.log(doctor);
+  console.log(doctor.specialties);
+  $('#doctor-info').append(`<h3> ${doctor.profile.first_name} ${doctor.profile.last_name} </h3> <h4>${doctor.specialties[0].description}</h4><li> ${doctor.profile.bio} </li> <li>${doctor.practices[0].description}</li><strong>Phone Number: ${doctor.practices[0].phones[0].number} </strong> <a href="${doctor.profile.image_url}">View Image</a>`);
 };
 
 
@@ -22,6 +25,7 @@ var displayDoctor = function(doctor) {
 $(document).ready(function(){
   var doctor = new Doctor();
   $('#submit-button').click(function(){
+    $('#error').empty();
     var state = $('#state').val();
     var userQuery = $('#query').val();
     doctor.getDoctors(state, userQuery, displayAllDoctors);
--- js/doctor.js
+++ js/doctor.js
@@ -6,13 +6,20 @@ function Doctor() {
 
 Doctor.prototype.getDoctors = function(state, userQuery, displayAllDoctors) {
   $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${userQuery}&location=${state}&skip=0&limit=10&user_key=${apiKey}`).then(function(response){
-    displayAllDoctors(response.data)
+    if (response.meta.count === 0) {
+      $('#error').text("There are no doctors found based on that keyword")
+      }
+    displayAllDoctors(response.data);
+  }).fail(function(error) {
+    $('#all-doctors').text(error.responseJSON.message);
   });
 };
 
 Doctor.prototype.findDoctor = function(doctorId, displayDoctor) {
   $.get(`https://api.betterdoctor.com/2016-03-01/doctors/npi/${doctorId}?user_key=${apiKey}`).then(function(response){
-    displayDoctor(response.data)
+    displayDoctor(response.data);
+  }).fail(function(error) {
+    $('#doctor-info').text(error.responseJSON.message);
   });
 };
 
