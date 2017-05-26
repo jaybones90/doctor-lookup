9bde146910f12f5f8dcf14c1ba9a3b604a87354e
Jason Ainsworth <jasoncruze@gmail.com>
2017-05-26 14:58:01 -0700

    add in display functionality to display a specific doctors info

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -4,7 +4,7 @@ var Doctor = require('./../js/doctor.js').doctorModule;
 var displayAllDoctors = function(allDoctors) {
   var doctor = new Doctor();
   allDoctors.forEach(function(doctor){
-    $('#doctor-info ul').append(`<li class="click-doctor" value="${doctor.npi}"> Doctor: ${doctor.profile.first_name} ${doctor.profile.last_name} </li> `);
+    $('#all-doctors ul').append(`<li class="click-doctor" value="${doctor.npi}"> Doctor: ${doctor.profile.first_name} ${doctor.profile.last_name} </li> `);
     });
     $('.click-doctor').click(function(){
       doctorId = $(this).val();
@@ -13,7 +13,8 @@ var displayAllDoctors = function(allDoctors) {
 };
 
 var displayDoctor = function(doctor) {
-  console.log(doctor);
+  console.log(doctor.practices[0].phones[0].number)
+  $('#doctor-info').append(`<h3> ${doctor.profile.first_name} ${doctor.profile.last_name} </h3> <li> ${doctor.profile.bio} </li> <li>Phone Number: ${doctor.practices[0].phones[0].number} </li> <a href="${doctor.profile.image_url}">View Image</a>`)
 };
 
 
